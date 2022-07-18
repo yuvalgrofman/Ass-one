77809bae6bf0c883aae6be057a6e8e2c018cbfe6
jonathan <jonathanmoiseyev@outlook.con>
2022-07-19 00:51:28 +0300

    flower class

---
+++ flower.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jonathan on 7/19/22.
+//
+
+#include "flower.h"
---
+++ flower.h
@@ -0,0 +1,8 @@
+//
+// Created by jonathan on 7/19/22.
+//
+
+#ifndef ASS_ONE_FLOWER_H
+#define ASS_ONE_FLOWER_H
+
+#endif //ASS_ONE_FLOWER_H
