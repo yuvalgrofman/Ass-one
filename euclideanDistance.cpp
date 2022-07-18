da0d0ad79200761408e98feaa6b64f62b5e35576
Yuval Grofman <yuvalgrofman@gmail.com>
2022-07-19 01:33:14 +0300

    commit

---
+++ euclideanDistance.cpp
@@ -0,0 +1,18 @@
+#include <distance.h>
+
+class euclideanDistance : private Distance
+{
+private:
+    /* data */
+public:
+    euclideanDistance(/* args */);
+    ~euclideanDistance();
+};
+
+euclideanDistance::euclideanDistance(/* args */)
+{
+}
+
+euclideanDistance::~euclideanDistance()
+{
+}
