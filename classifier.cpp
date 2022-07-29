9444779daed3da8ec87c1fdb1df5790751765316
yuval <yuvalgrofman@gmail.com>
2022-07-29 18:02:42 +0300

    Added classifier

---
+++ classifier.cpp
@@ -0,0 +1,35 @@
+#include "classifier.h"
+
+
+Classifier::Classifier(int k, vector<Distance>* distances, string classifiedData, string unclassifiedData) : k(k),
+distances(distances) {
+
+    FlowerReader &unClassifiedReader = *(new FlowerReader(unclassifiedData));
+    FlowerPoint* ptr = unClassifiedReader.readFlowerPoint();
+
+    while (ptr != nullptr) {
+        unclassifiedFLowers->push_back(*(ptr));
+        ptr = unClassifiedReader.readFlowerPoint();
+    }
+
+    FlowerReader &classifiedReader = *(new FlowerReader(classifiedData));
+    DataSpaceCreator creator = DataSpaceCreator(classifiedReader);
+    dataSpace = &creator.makeDataSpace();
+}
+
+void Classifier::classify() {
+    for (Distance distance : *distances) {
+        predictFileByDist(distance.getName() + "_output.csv", distance);
+    }
+}
+
+void Classifier::predictFileByDist(string outputFile, Distance distance) {
+    ofstream outfile;
+    outfile.open(outputFile, ios::out);
+
+    for (FlowerPoint flower : *unclassifiedFLowers) {
+        outfile << dataSpace->predict(k, flower, distance) << endl;
+    }
+
+    outfile.close();
+}
---
+++ classifier.h
@@ -0,0 +1,27 @@
+#ifndef ASS_ONE_CLASSIFIER_H
+#define ASS_ONE_CLASSIFIER_H
+
+#include <string>
+#include "Distance.h"
+#include "vector"
+#include "DataSpaceCreator.h"
+#include <iostream>
+
+using namespace std;
+
+class Classifier {
+    private:
+        int k;
+        vector<Distance>* distances;
+        DataSpace* dataSpace;
+        vector<FlowerPoint>* unclassifiedFLowers = new vector<FlowerPoint>();
+
+        void predictFileByDist(string outputFile, Distance distance);
+
+    public:
+        Classifier(int k, vector<Distance>* distances, string classifiedData, string unclassifiedData);
+        void classify();
+};
+
+
+#endif //ASS_ONE_CLASSIFIER_H
