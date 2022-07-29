472c0208d7e7d94f18c629d575adc21023c93bee
yuval <yuvalgrofman@gmail.com>
2022-07-29 18:01:31 +0300

    Added classifier and changed all distance funcs to virtual

--- manhattanDistance.cpp
+++ manhattanDistance.cpp
@@ -8,3 +8,7 @@ double ManhattanDistance::getDistance(const FlowerPoint p1, const FlowerPoint p2
            abs(p1.getSepalWidth() - p2.getSepalWidth()) +
            abs(p1.getWidth() - p1.getWidth());
 }
+
+string ManhattanDistance::getName() const {
+    return "manhattan";
+}
