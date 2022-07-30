76af95232060aa7bb4b6022765765c5ae2774eff
yuval <yuvalgrofman@gmail.com>
2022-07-30 19:29:54 +0300

    changed file path to actually be relative

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[]) {
     vector<Distance*> distances = {(new EuclideanDistance()), (new ManhattanDistance()), (new ChebyshevDistance())};
-    Classifier* classifier = new Classifier(3, &distances, "..\\classified.csv","..\\Unclassified.csv");
+    Classifier* classifier = new Classifier(3, &distances, "../classified.csv","../Unclassified.csv");
 
     classifier->classify();
 }
