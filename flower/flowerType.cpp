549237c66bfc6bb2d0c51f7c50b6fa32bc03dfda
yuval <yuvalgrofman@gmail.com>
2022-07-31 22:20:34 +0300

    fixed typo

--- flower/flowerType.cpp
+++ flower/flowerType.cpp
@@ -5,7 +5,7 @@ std::ostream& operator<<(std::ostream& os, const FlowerType Token) {
             os << "Iris-setosa";
             break;
         case FlowerType::VIRGINICA:
-            os << "Iris-viriginica";
+            os << "Iris-virginica";
             break;
         case FlowerType::VERSICOLOR:
             os << "Iris-versicolor";
