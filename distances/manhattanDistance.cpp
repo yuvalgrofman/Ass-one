f75c299426523a2996870778c1f7a19e7f148641
yuval <yuvalgrofman@gmail.com>
2022-08-01 20:35:20 +0300

    Fixed manhattanDistance.cpp getDistance function truly

--- distances/manhattanDistance.cpp
+++ distances/manhattanDistance.cpp
@@ -3,10 +3,10 @@
 
 
 double ManhattanDistance::getDistance(const FlowerPoint& p1, const FlowerPoint& p2) const {
-    return max(abs(p1.getPetalLength() - p2.getPetalLength()),
-           max(abs(p1.getSepalLength() - p2.getSepalLength()),
-           max(abs(p1.getSepalWidth() - p2.getSepalWidth()),
-           abs(p1.getWidth() - p2.getWidth()))));
+    return abs(p1.getPetalLength() - p2.getPetalLength()) +
+           abs(p1.getSepalLength() - p2.getSepalLength()) +
+           abs(p1.getSepalWidth() - p2.getSepalWidth()) +
+           abs(p1.getWidth() - p1.getWidth());
 }
 
 string ManhattanDistance::getName() const {
